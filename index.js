0bc7f6b96aa878bd0d251dfcfec582a574a9f065
Cristiano <cristianochincarosanches@gmail.com>
2025-08-23 00:11:06 -0300

    add_server_recovery_and_send_message_in_local_machine

--- index.js
+++ index.js
@@ -1,34 +1,73 @@
 import express from 'express';
+
 import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
-import { Server } from 'socket.io'
 
+import { Server } from 'socket.io';
+
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+//
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
-
+const io = new Server(server,{
+    connectionStateRecovery:{}
+});
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
+// Database
+const db = await open({
+    filename : 'chat.db',
+    driver : sqlite3.Database
 });
 
-io.on('connection', (socket) => {
-    console.log('new connection found');
-    socket.on('chat message', (msg) => {
-        console.log(msg);
-    });
+await db.exec(`
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_offset TEXT UNIQUE,
+    content TEXT
+);`
+);
 
-    socket.on('disconnect', ()=>{
-        console.log("user disconnect");
-    });
+// App route
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'src/index.html'));
 });
 
 app.get('/login', (req, res) => {
     res.sendFile(join(__dirname, 'auth/login.html'));
 });
 
+// Init server
 server.listen(3000, () => {
     console.log("Server up");
+});
+
+// Server operations
+io.on('connection', (socket) => {
+    socket.on('chat message', async (msg) => {
+        let result;
+        try {
+            result=await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+        } catch (e){
+            return;
+        }
+        
+        io.emit('chat message', msg, result.lastID);
+    });
+
+    if(!socket.recovered){
+        try{
+            db.each('SELECT id, content FROM messages WHERE id > ?',
+                [socket.handshake.auth.severOffset || 0],
+                (_err, row) => {
+                    socket.emit('chat message', row.content, row.id);
+                }
+            );
+        } catch (e){
+            console.log("Something goes wrong");
+        }
+    }
 });
